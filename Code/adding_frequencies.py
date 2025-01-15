767589b3ea82063d84122ca374e251ffa29f83ff
Ali Al-Azem <avalazem@gmail.com>
2025-01-15 16:17:04 +0100

    Initial commit with folder contents

---
+++ Code/adding_frequencies.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+training_df = pd.read_json(r'C:/Users/ali_a/Desktop/Single Word Processing Stage/SSP Human Error/Data/train_phonemes.json')
+
+ccv_df=pd.read_csv(r'C:/Users/ali_a/Desktop/Single Word Processing Stage/SSP Human Error/Code/SSP_CVV_combinations.py')
+vcc_df=pd.read_csv(r'C:/Users/ali_a/Desktop/Single Word Processing Stage/SSP Human Error/Code/SSP_VCC_combinations.py')
+ccvcc_df=pd.read_csv(r'C:/Users/ali_a/Desktop/Single Word Processing Stage/SSP Human Error/Code/SSP_CCVCC_combinations.py')
+
+# Check frequencies for C1 C2
+# Check frequencies for C1 C2
+
+# Check frequencies for  C4
+# Check frequencies for V C C
+
+# Check frequencies for C1 C2
